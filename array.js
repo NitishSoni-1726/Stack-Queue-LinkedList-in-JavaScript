8e6fb372fd09f67d76e5f20543c9b450351864ba
Nitish Soni <nitishsoni890@gmail.com>
2023-07-12 11:00:04 +0530

    Implemented array reverse in place

---
+++ array.js
@@ -0,0 +1,8 @@
+function reverse(array) {
+  for (let i = 0; i < Math.trunc(array.length / 2); i++) {
+    let temp = array[i];
+    array[i] = array[array.length - (i + 1)];
+    array[array.length - (i + 1)] = temp;
+  }
+  return array;
+}
